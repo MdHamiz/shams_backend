aedc20dd9cea2e961cbb9586f55a8e45f9a2b059
Md Hamiz Imam <mdhamizimam@gmail.com>
2025-09-02 00:15:38 +0530

    edit

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ app.get("/", (req, res) => {
 
 app.use("/api", formRouter);
 
+app.use((req, res, next) => {
+  console.log("Request received:", req.method, req.url);
+  next();
+});
+
+
 app.listen(PORT, () => {
     console.log(`App is running on port: ${PORT}`);
 });
